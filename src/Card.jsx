3abf3744f80097d6a91addf89967408eb2b9c48e
abhinay90 <pratapsinghabhinay17@gmail.com>
2024-03-30 18:48:46 +0530

    check commit again

--- src/Card.jsx
+++ src/Card.jsx
@@ -17,7 +17,7 @@ const Card = () => {
   const getNextData = async () => {
     setIsLoading(true);
     key.current = currentActivity.key;
-    const res = await axios("https://jsonplaceholder.typicode.com/posts");
+    const res = await axios("http://www.boredapi.com/api/activity");
     setIsLoading(false);
     setCurrentActivity(res.data);
   };
